8060b49cb106ba25719578404dc91938ab9a212f
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com>
2025-09-15 15:16:18 -0300

    Atualização de Metadados

---
+++ Test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+""" Python Shell
+
+Esse programa tem como finalidade testar os conhecimentos adquiridos em Python """
+
+
+__version__ = "0.0.1"
+__author__ = "Rafael Silva Moura"
+__license__ = "no license"
+
+
+
+
+
