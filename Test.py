07ec5f6b3cbccd823db82eb3e47ef77fe0956457
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com>
2025-09-15 16:05:25 -0300

    Subindo

--- Test.py
+++ Test.py
@@ -1,14 +1,35 @@
-#! /usr/bin/env python3
-
+#!/usr/bin/env python3
 """ Python Shell
 
-Esse programa tem como finalidade testar os conhecimentos adquiridos em Python """
-
+Esse programa tem como finalidade testar os conhecimentos adquiridos em Python
+"""
 
 __version__ = "0.0.1"
 __author__ = "Rafael Silva Moura"
 __license__ = "no license"
 
+
+import os
+
+current_language = "en_US"
+msg = "Hello World"
+
+language = os.getenv("LANG", "en_US") # Add a default language ( English )
+
+if language == current_language:
+    print(msg)
+
+elif language == "pt_BR":
+    print("Olá! Mundo!")
+
+elif language == "fr_FR":
+    print("Bonjour! Le Monde!")
+
+elif language == "it_IT":
+    print("Ciao! Mondo!")
+
+else:
+    print(msg)
 
 
 
