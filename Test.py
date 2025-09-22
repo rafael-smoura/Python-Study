2be82861866122ee277f06f02e086b5b9f16faa9
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com.>
2025-09-22 13:07:47 -0300

    Aprimoramento da resiliência do programa

--- Test.py
+++ Test.py
@@ -1,27 +1,62 @@
-#!/home/rsm7/Python-Study/dependentes/bin/env python3
-""" Python Shell
+#!/home/rsm7/Python-Study/dependentes/bin/python3
+""" Hello World Multi Línguas
 
-Esse programa tem como finalidade testar os conhecimentos adquiridos em Python
+Dependendo da lingua configurada no ambiwnrw o programa exibe a mensagem correspondente.
+
+como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Ou informe através do CLI arguments `--lang`
+
+Ou o usuário terá que digitar.
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.4"
 __author__ = "Rafael Silva Moura"
-__license__ = "no license"
-
+__license__ = "Unlicense"
 
 import os
-frases = {"pt_BR": "Olá! Mundo!",
+import sys
+
+arguments = { "lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+    # TODO: ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option '{key}'")
+        sys.exit()
+    arguments[key] = value
+   
+current_language = arguments["lang"]
+if current_language is None:
+    current_language = os.getenv("LANG")
+    # TODO: Usar repetição
+   
+    if current_language is None or current_language == "C.UTF-8":
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
+
+text = {"pt_BR": "Olá! Mundo!",
           "fr_FR": "Bonjour! Le Monde!",
           "it_IT": "Ciao! Mondo!",
           "en_US": "Hello World" }
 
-language = "pt_BR"
-print(language)
+print((text[current_language] + "\n")*int(arguments["count"]))
 
-if language in frases:
-        print(frases[language])
-else:
-        print(frases["en_US"])
     
     
     
